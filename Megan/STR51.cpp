ff559575dd7729f7ea75bf114f05f54cc7a91060
mlefebv <146863012+mlefebv@users.noreply.github.com>
2023-10-15 17:56:17 -0500

    STR51.cpp

--- Megan/STR51.cpp
+++ Megan/STR51.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <string>
-#include <cstdlib> // for std::getenv
 
 int main() {
-    // Retrieve the getenv path
-    const char* tmpVal = std::getenv("PATH");
+    char* charPointer = nullptr;
 
-    // Create an std::string 'tmp' based on the environment variable value
-    std::string tmp = (tmpVal != nullptr) ? tmpVal : "";
-
-    // Check if 'tmp' is not empty (i.e., 'TMP' exists and has a value)
-    if (!tmp.empty()) {
-        std::cout << "TMP environment variable value: " << tmp << std::endl;
+    // Check if the pointer is null before creating a string
+    if (charPointer != nullptr) {
+        std::string str(charPointer); // Create string from the pointer since it isnt null
+        std::cout << "String: " << str << std::endl;
     } else {
-        std::cout << "TMP environment variable is not set or is empty." << std::endl;
+        std::cout << "Cannot create std::string from a null pointer." << std::endl;
     }
-      return 0;
+
+    return 0;
 }
