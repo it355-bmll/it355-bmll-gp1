10b2aba09146aea08a0bf2e5a56c466209233dcd
mlefebv <146863012+mlefebv@users.noreply.github.com>
2023-10-15 18:09:16 -0500

    Update ERR59.cpp

--- Megan/ERR59.cpp
+++ Megan/ERR59.cpp
@@ -3,16 +3,14 @@
 int func() noexcept(true) {
   int val = 10;
   if (val < 2)  {
-    return 42;
+    return 42; //shows that an error occurred without throwing exception
   }
-  // ...
-  return 0;
+  return 0; //program runs normally
 }
 
 
 void f() {
   if (int err = func()) {
-    // Handle error
     std::cerr << "error occurred" << err << std::endl;
   }
   else{
