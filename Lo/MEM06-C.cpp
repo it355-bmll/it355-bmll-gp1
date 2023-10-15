f694694bc03cc23375690e0c6fed6d98c38f3e59
lsnorsw <145047841+lsnorsw@users.noreply.github.com>
2023-10-15 23:56:23 +0000

    Add files via upload

--- Lo/MEM06-C.cpp
+++ Lo/MEM06-C.cpp
@@ -1,20 +1,9 @@
-/**
- * @file MEM06-C.cpp
- * @brief A C++ program that demonstrates compliance with the recommendation MEM06-C. This code creates an object of class SecretData,
- * stores some secret data, and ensures the secret data can be displayed without writing it to the disk.
- */
-
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <sys/resource.h>
 //Setting size of core dump to 0 to ensure no data accidentally leaks out
-struct rlimit limit;
-limit.rlim_cur = 0;
-limit.rlim_max = 0;
-if (setrlimit(RLIMIT_CORE, &limit) != 0) {
-    std::cout << "HELP ERROR";
-}
+
 
 class SecretData {
 public:
@@ -29,11 +18,20 @@ private:
     std::string data_;
 };
 
-void main() {
+int main() {
+    
+    struct rlimit limit;
+    limit.rlim_cur = 0;
+    limit.rlim_max = 0;
+    if (setrlimit(RLIMIT_CORE, &limit) != 0) {
+    std::cout << "HELP ERROR";
+    }
+    
     std::string sensitiveData = "This is sensitive information.";
     SecretData SecretData(sensitiveData);
 
     // Printing sensitive data without writing it to disk
     SecretData.PrintData();
-
+    
+    return 0;
 }
